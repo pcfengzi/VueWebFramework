6cefad009671401ff80696fe885faf1df2913499
pcfengzi <626274511@qq.com>
2020-05-08 17:37:00 +0800

    Add files of Initialization

---
+++ src/components/EnterPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="CopClass">
+    <h1>{{ msg }}</h1>
+    <h2>Element UI Button</h2>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CopName',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
